3d21cf5fa7d3997ccefc2fca7a3a51c745f7e4af
Arne Coucheron <arco68@gmail.com>
2020-11-15 01:51:38 +0100

    media: Switch to display_headers lib include
    LOCAL_COPY_HEADERS is deprecated and Qualcomm HALs have been
    updated to remove its usage. As a consequence, the display headers
    are no longer copied to TARGET_OUT_HEADERS, so use the equivalent
    header lib instead.

--- mm-video-v4l2/vidc/common/Android.mk
+++ mm-video-v4l2/vidc/common/Android.mk
@@ -32,7 +32,6 @@ endif
 libmm-vidc-inc      += $(call project-path-for,qcom-media)/mm-core/inc
 libmm-vidc-inc      += $(call project-path-for,qcom-media)/mm-video-v4l2/vidc/vdec/inc
 libmm-vidc-inc      += $(call project-path-for,qcom-media)/mm-video-v4l2/vidc/venc/inc
-libmm-vidc-inc      += $(TARGET_OUT_HEADERS)/qcom/display
 libmm-vidc-inc      += $(call project-path-for,qcom-media)/libc2dcolorconvert
 libmm-vidc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
