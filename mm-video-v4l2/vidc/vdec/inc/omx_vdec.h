5278355905d15c938d447048c852ce3b390997f7
Michael Bestas <mkbestas@lineageos.org>
2025-01-09 09:19:20 +0200

    mm-video-v4l2: vidc: Drop support for hypervisor
    This isn't used by any supported platform
    
    Change-Id: I09de8028e4b0f857286fa51974431c7d93c9fdd4

--- mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
+++ mm-video-v4l2/vidc/vdec/inc/omx_vdec.h
@@ -1559,7 +1559,6 @@ class omx_vdec: public qc_omx_component
         unsigned int m_fill_output_msg;
         bool client_set_fps;
         unsigned int stereo_output_mode;
-        bool m_hypervisor;
         class allocate_color_convert_buf
         {
             public:
